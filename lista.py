92f7cc8652a4b4e96e79f57eecc2f84e6ed1c727
Jakub Piskorz <kpiskorz13@gmail.com>
2021-10-20 22:08:19 +0200

    Add for loop

--- lista.py
+++ lista.py
@@ -2,3 +2,6 @@ zakupy = {
     "piekarnia": ["chleb", "bułki", "pączek"],
     "warzywniak": ["marchew", "seler", "Rukola"]
 }
+for sklep, rzeczy in zakupy.items():
+    x = f"Idę do {sklep}, kupuję tu następujące rzeczy {rzeczy}"
+    print(x)
