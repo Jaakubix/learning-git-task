1639691bb3cd57f0208a25fa2ac0d326cfb0b187
Jakub Piskorz <kpiskorz13@gmail.com>
2021-10-20 22:19:00 +0200

    Add function to make first letter of store name and shopping list capital

--- lista.py
+++ lista.py
@@ -3,5 +3,9 @@ zakupy = {
     "warzywniak": ["marchew", "seler", "Rukola"]
 }
 for sklep, rzeczy in zakupy.items():
+    sklep = sklep.capitalize()
+    rzeczy[0] = rzeczy[0].capitalize()
+    rzeczy[1] = rzeczy[1].capitalize()
+    rzeczy[2] = rzeczy[2].capitalize()
     x = f"Idę do {sklep}, kupuję tu następujące rzeczy {rzeczy}"
     print(x)
