75b44ced9bfc0c9fd1487c8031a5a7a23addac49
Jakub Piskorz <kpiskorz13@gmail.com>
2021-10-20 22:20:02 +0200

    Add string that shows how many items i bought

--- lista.py
+++ lista.py
@@ -7,5 +7,8 @@ for sklep, rzeczy in zakupy.items():
     rzeczy[0] = rzeczy[0].capitalize()
     rzeczy[1] = rzeczy[1].capitalize()
     rzeczy[2] = rzeczy[2].capitalize()
+    wartosc.append(rzeczy)
     x = f"Idę do {sklep}, kupuję tu następujące rzeczy {rzeczy}"
     print(x)
+wartosc1 = len(wartosc[0]) + len(wartosc[1])
+print(f"W sumie kupuję {wartosc1} produktów.")
